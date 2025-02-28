111233a0075492a8a39525e87bf72b8e98ed6d0a
jl <orionjl1106@gmail.com>
2025-02-28 08:47:40 +0800

    login error fix

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,67 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Database from '@tauri-apps/plugin-sql';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import MainLayout from "@/layouts/MainLayout";
+import { IoMdMail } from "react-icons/io";
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaLock } from "react-icons/fa";
 
-const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+export default function Login() {
+  return (
+    <>
+      <MainLayout>
+        <h1 className="text-3xl font-semibold text-center mb-16">Login</h1>
+        <form action="" className="space-y-5">
+          <Input type="email" placeholder="Email" icon={<IoMdMail />} />
+          <Input type="password" placeholder="Password" icon={<FaLock />} />
+          <Button type="submit" className="w-full my-6 cursor-pointer" size="lg">
+            Login
+          </Button>
+        </form>
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        // Simulate login logic
-        if (email && password) {
-            try {
-                const db = await Database.load('sqlite:database.db');
-                const result = await db.select("SELECT * FROM users WHERE email = $1", [email]);
-                if (result[0].password === password) {
-                    navigate("/todo", {state: {user_id: result[0].id}})
-                }
-            } catch (error) {
-                console.log(error)
-            }
-        } else {
-            setError('Invalid email or password');
-        }
-    };
-
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="bg-white p-8 rounded-lg shadow-md w-96">
-                <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-sm font-medium text-gray-700" htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-sm font-medium text-gray-700" htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
-                        />
-                    </div>
-                    {error && <div className="mb-4 text-red-500">{error}</div>}
-                    <button type="submit" className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">
-                        Login
-                    </button>
-                </form>
-                <a href="/signup" className='mt-5 text-teal-500'>don't have an account?</a>
-            </div>
-        </div>
-    );
-};
-
-export default Login;
+        <p className="text-center">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-600 hover:underline">
+            Sign up
+          </Link>
+        </p>
+      </MainLayout>
+    </>
+  );
+}
