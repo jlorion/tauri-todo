a8482970ba0d46fe3fe6e5104d7a3ecc3164a29e
mrkkjfrsn <marckie.js@gmail.com>
2025-03-11 22:48:09 +0800

    delete uncessary classes and routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Todo from "./pages/Todo";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import AddTask from "./pages/AddTask";
-import Completed from "./pages/Completed";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Todo />} />
-        <Route path="/addtask" element={<AddTask />} />
-        <Route path="/completed" element={<Completed />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
